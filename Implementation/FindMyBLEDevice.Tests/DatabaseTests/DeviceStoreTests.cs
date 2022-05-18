07be0a6250c514cf93a9bc31279c95f524d99112
Nicolas Stellwag <nico.stellwag@gmail.com>
2022-05-18 17:11:31 +0200

    finish DevicesStore tests
    Signed-off-by: Nicolas Stellwag <nico.stellwag@gmail.com>

--- Implementation/FindMyBLEDevice.Tests/DatabaseTests/DeviceStoreTests.cs
+++ Implementation/FindMyBLEDevice.Tests/DatabaseTests/DeviceStoreTests.cs
@@ -4,6 +4,7 @@ using FindMyBLEDevice.Services.Database;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FindMyBLEDevice.Tests
@@ -28,7 +29,7 @@ namespace FindMyBLEDevice.Tests
             db.Verify(mock => mock.SaveDeviceAsync(It.Is<BTDevice>(device =>
                 device.BT_id == bt_id
                 && device.Name == name
-            )), Times.Once);
+                )), Times.Once);
             Assert.IsNull(result.Exception);
         }
 
@@ -71,8 +72,9 @@ namespace FindMyBLEDevice.Tests
             // assert
             db.Verify(mock => mock.GetDeviceAsync(It.Is<int>(arg => arg == id)), Times.Once);
             db.Verify(mock => mock.SaveDeviceAsync(It.Is<BTDevice>(device =>
-            device.Id == id
-            && device.Name.Equals(newName))), Times.Once);
+                device.Id == id
+                && device.Name.Equals(newName))),
+                Times.Once);
             Assert.IsNull(result.Exception);
         }
 
@@ -115,6 +117,267 @@ namespace FindMyBLEDevice.Tests
 
             // assert
             Assert.IsTrue(result.Exception?.InnerException is DeviceStoreException);
+        }
+
+        [TestMethod]
+        public void UpdateDeviceLocation_CallsDbMethodsCorrectly()
+        {
+            // arrange
+            const int id = 1;
+            const int oldVals = 0;
+            const int newVals = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult(new BTDevice()
+            {
+                Id = id,
+                LastGPSLatitude = oldVals,
+                LastGPSLongitude = oldVals
+            }));
+            db.Setup(mock => mock.SaveDeviceAsync(It.IsAny<BTDevice>())).Returns(Task.FromResult(1));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.UpdateDeviceLocation(id, newVals, newVals);
+
+            // assert
+            db.Verify(mock => mock.GetDeviceAsync(It.Is<int>(arg => arg == id)), Times.Once);
+            db.Verify(mock => mock.SaveDeviceAsync(It.Is<BTDevice>(device =>
+                device.Id == id
+                && device.LastGPSLongitude.Equals(newVals)
+                && device.LastGPSLatitude.Equals(newVals)
+                && (device.LastGPSTimestamp - DateTime.Now).TotalMilliseconds < 5)),
+                Times.Once);
+            Assert.IsNull(result.Exception);
+        }
+
+        [TestMethod]
+        public void UpdateDeviceLocation_NoDeviceWithIdThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            const int newVals = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice?>(null));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.UpdateDeviceLocation(id, newVals, newVals);
+
+            // assert
+            Assert.IsTrue(result.Exception?.InnerException is ArgumentException);
+            db.Verify(mock => mock.SaveDeviceAsync(It.IsAny<BTDevice>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void UpdateDeviceLocation_UpdatingFailsThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            const int oldVals = 0;
+            const int newVals = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult(new BTDevice()
+            {
+                Id = id,
+                LastGPSLatitude = oldVals,
+                LastGPSLongitude = oldVals
+            }));
+            db.Setup(mock => mock.SaveDeviceAsync(It.IsAny<BTDevice>())).Returns(Task.FromResult(0));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.UpdateDeviceLocation(id, newVals, newVals);
+
+            // assert
+            Assert.IsTrue(result.Exception?.InnerException is DeviceStoreException);
+        }
+
+        [TestMethod]
+        public void UpdateDeviceActive_CallsDbMethodsCorrectly()
+        {
+            // arrange
+            const int id = 1;
+            const bool oldActive = false;
+            const bool newActive = true;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult(new BTDevice()
+            {
+                Id = id,
+                Active = oldActive
+            }));
+            db.Setup(mock => mock.SaveDeviceAsync(It.IsAny<BTDevice>())).Returns(Task.FromResult(1));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.UpdateDeviceActive(id, newActive);
+
+            // assert
+            db.Verify(mock => mock.GetDeviceAsync(It.Is<int>(arg => arg == id)), Times.Once);
+            db.Verify(mock => mock.SaveDeviceAsync(It.Is<BTDevice>(device =>
+                device.Id == id
+                && device.Active.Equals(newActive))),
+                Times.Once);
+            Assert.IsNull(result.Exception);
+        }
+
+        [TestMethod]
+        public void UpdateDeviceActive_NoDeviceWithIdThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            const bool newActive = true;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice?>(null));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.UpdateDeviceActive(id, newActive);
+
+            // assert
+            Assert.IsTrue(result.Exception?.InnerException is ArgumentException);
+            db.Verify(mock => mock.SaveDeviceAsync(It.IsAny<BTDevice>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void UpdateDeviceActive_UpdatingFailsThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            const bool oldActive = false;
+            const bool newActive = true;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult(new BTDevice()
+            {
+                Id = id,
+                Active = oldActive
+            }));
+            db.Setup(mock => mock.SaveDeviceAsync(It.IsAny<BTDevice>())).Returns(Task.FromResult(0));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.UpdateDeviceActive(id, newActive);
+
+            // assert
+            Assert.IsTrue(result.Exception?.InnerException is DeviceStoreException);
+        }
+
+        [TestMethod]
+        public void DeleteDevice_CallsDbMethodsCorrectly()
+        {
+            // arrange
+            const int id = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice?>(new BTDevice()
+            {
+                Id = id
+            }));
+            db.Setup(mock => mock.DeleteDeviceAsync(It.IsAny<BTDevice>())).Returns(Task.FromResult(1));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.DeleteDevice(id);
+
+            // assert
+            db.Verify(mock => mock.GetDeviceAsync(It.Is<int>(arg => arg == id)), Times.Once);
+            db.Verify(mock => mock.DeleteDeviceAsync(It.Is<BTDevice>(device =>
+                device.Id == id)),
+                Times.Once);
+            Assert.IsNull(result.Exception);
+        }
+
+        [TestMethod]
+        public void DeleteDevice_NoDeviceWithIdThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice?>(null));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.DeleteDevice(id);
+
+            // assert
+            Assert.IsTrue(result.Exception?.InnerException is ArgumentException);
+            db.Verify(mock => mock.DeleteDeviceAsync(It.IsAny<BTDevice>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void DeleteDevice_DeletingFailsThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice?>(new BTDevice()
+            {
+                Id = id
+            }));
+            db.Setup(mock => mock.DeleteDeviceAsync(It.IsAny<BTDevice>())).Returns(Task.FromResult(0));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task result = store.DeleteDevice(id);
+
+            // assert
+            Assert.IsTrue(result.Exception?.InnerException is DeviceStoreException);
+        }
+
+        [TestMethod]
+        public void GetDevice_CallsDbMethodsCorrectly()
+        {
+            // arrange
+            const int id = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice>(new BTDevice()
+            {
+                Id = id
+            }));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task<BTDevice> result = store.GetDevice(id);
+            result.Wait();
+
+            // assert
+            Assert.IsNull(result.Exception);
+            db.Verify(mock => mock.GetDeviceAsync(It.Is<int>(arg => arg == id)), Times.Once);
+            Assert.AreEqual(id, result.Result.Id);
+        }
+
+        [TestMethod]
+        public void GetDevice_NoDeviceWithIdThrowsCorrectException()
+        {
+            // arrange
+            const int id = 1;
+            var db = new Mock<IDatabase>();
+            db.Setup(mock => mock.GetDeviceAsync(It.IsAny<int>())).Returns(Task.FromResult<BTDevice?>(null));
+            var store = new DevicesStore(db.Object);
+
+            // act
+            Task<BTDevice> result = store.GetDevice(id);
+            try
+            {
+                result.Wait();
+            } catch (AggregateException ae)
+            {
+                // assert 
+                Assert.IsTrue(ae.InnerException is ArgumentException);
+            }
+        }
+
+        [TestMethod]
+        public void GetAllDevices_CallsDbMethodCorrectly()
+        {
+            // arrange
+            var db = new Mock<IDatabase>();
+            var store = new DevicesStore(db.Object);
+            
+            // act 
+            Task<List<BTDevice>> result = store.GetAllDevices();
+
+            // assert
+            Assert.IsNull(result.Exception);
+            db.Verify(mock => mock.GetAllDevicesAsync(), Times.Once);
         }
     }
 }
