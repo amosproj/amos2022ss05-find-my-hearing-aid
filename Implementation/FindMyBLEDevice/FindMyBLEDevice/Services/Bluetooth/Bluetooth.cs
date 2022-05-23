c926b394bc6797d261ae37344ceb3b527280d22a
Dominik Pysch <domi.pysch@gmail.com>
2022-05-23 22:36:04 +0200

    work on bluetooth-finder-screen
    Co-authored-by: Nicolas Stellwag <nico.stellwag@gmail.com>
    Signed-off-by: Dominik Pysch <domi.pysch@gmail.com>

--- Implementation/FindMyBLEDevice/FindMyBLEDevice/Services/Bluetooth/Bluetooth.cs
+++ Implementation/FindMyBLEDevice/FindMyBLEDevice/Services/Bluetooth/Bluetooth.cs
@@ -1,38 +1,46 @@
-﻿using FindMyBLEDevice.Models;
+﻿// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Dominik Pysch <dominik.pysch@fau.de>
+// SPDX-FileCopyrightText: 2022 Nicolas Stellwag <nicolas.stellwag@fau.de>
+
+using FindMyBLEDevice.Models;
 using Plugin.BLE;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FindMyBLEDevice.Services.Bluetooth
 {
     public class Bluetooth
     {
-        private List<AvailableBTDevice> deviceList;
+        private readonly List<AvailableBTDevice> deviceList;
 
         public Bluetooth()
         {
             deviceList = new List<AvailableBTDevice>();
         }
 
-        public async Task search(int scanTimeout)
+        public async Task Search(int scanTimeout)
         {
-            deviceList = new List<AvailableBTDevice>();
-            var adapter = CrossBluetoothLE.Current.Adapter;
 
-            adapter.DeviceDiscovered += async (s, a) => {
-                deviceList.Add(new AvailableBTDevice()
+            deviceList.Clear();
+            Plugin.BLE.Abstractions.Contracts.IAdapter adapter = CrossBluetoothLE.Current.Adapter;
+
+            adapter.DeviceDiscovered += (s, a) => {
+
+                if(!deviceList.Exists(o => o.Id == a.Device.Id))
                 {
-                    Name = a.Device.Name,
-                    Id = a.Device.Id,
-                    Rssi = a.Device.Rssi
-                });
+                    deviceList.Add(new AvailableBTDevice()
+                    {
+                        Name = a.Device.Name ?? "Unknown Device",
+                        Id = a.Device.Id,
+                        Rssi = a.Device.Rssi
+                    });
+                }
+
             };
 
             adapter.ScanTimeout = scanTimeout;
-
             await adapter.StartScanningForDevicesAsync();
+
         }
 
         public List<AvailableBTDevice> GetAvailableDevices()
--- Implementation/FindMyBLEDevice/FindMyBLEDevice/ViewModels/BluetoothViewModel.cs
+++ Implementation/FindMyBLEDevice/FindMyBLEDevice/ViewModels/BluetoothViewModel.cs
@@ -1,9 +1,11 @@
-﻿using FindMyBLEDevice.Models;
-using System;
+﻿// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Dominik Pysch <dominik.pysch@fau.de>
+// SPDX-FileCopyrightText: 2022 Nicolas Stellwag <nicolas.stellwag@fau.de>
+
+using FindMyBLEDevice.Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Xamarin.Forms;
 
 namespace FindMyBLEDevice.ViewModels
@@ -13,24 +15,35 @@ namespace FindMyBLEDevice.ViewModels
         public ObservableCollection<AvailableBTDevice> AvailableDevices { get; }
         public Command LoadDevicesCommand { get; }
         public Command SearchDevicesCommand { get; }
+
         public BluetoothViewModel()
         {
-            Title = "Available BT Devices";
+            Title = "Available BLE Devices";
             AvailableDevices = new ObservableCollection<AvailableBTDevice>();
-            LoadDevicesCommand = new Command(async () => await ExecuteLoadDevicesCommand());
-            SearchDevicesCommand = new Command(async () => await ExecuteSearchDevicesCommand());
+            LoadDevicesCommand = new Command(() => ExecuteLoadDevicesCommand());
+            SearchDevicesCommand = new Command(() => ExecuteSearchDevicesCommand());
         }
 
-        private async Task ExecuteLoadDevicesCommand()
+        private void ExecuteLoadDevicesCommand()
         {
             AvailableDevices.Clear();
             List<AvailableBTDevice> dev = App.Bluetooth.GetAvailableDevices();
+            dev.Sort((x, y) => y.Rssi.CompareTo(x.Rssi));
             dev.ForEach(AvailableDevices.Add);
         }
 
-        private async Task ExecuteSearchDevicesCommand()
+        private void ExecuteSearchDevicesCommand()
         {
-            await App.Bluetooth.search(5000);
+
+            int period = 10000;
+
+            TimerCallback timerDelegate = new TimerCallback(async o => {
+                await App.Bluetooth.Search(5000);
+                ExecuteLoadDevicesCommand();
+                OnPropertyChanged("AvailableDevices");
+            });
+            Timer timer = new Timer(timerDelegate, null, 0, period);
+
         }
 
     }
