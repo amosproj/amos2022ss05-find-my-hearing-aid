f6bb4e46c4dd46b22326c0602d0c93d82f91a3d4
Nicolas Stellwag <nico.stellwag@gmail.com>
2022-05-31 14:34:43 +0200

    extract bluetooth interface, create bluetooth test class
    Signed-off-by: Nicolas Stellwag <nico.stellwag@gmail.com>

---
+++ Implementation/FindMyBLEDevice.Tests/BluetoothTests/BluetoothTests.cs
@@ -0,0 +1,13 @@
+﻿﻿// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Nicolas Stellwag <nicolas.stellwag@fau.de>
+
+using FindMyBLEDevice.Services.Bluetooth;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FindMyBLEDevice.Tests.BluetoothTests
+{
+    internal class BluetoothTests
+    {
+    }
+}
