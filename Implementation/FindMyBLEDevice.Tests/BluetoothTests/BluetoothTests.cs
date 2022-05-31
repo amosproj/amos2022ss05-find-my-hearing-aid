e38b680435017f615ba50d30f74014d27c4273d4
Nicolas Stellwag <nico.stellwag@gmail.com>
2022-05-31 18:42:02 +0200

    add first bluetooth test
    Signed-off-by: Nicolas Stellwag <nico.stellwag@gmail.com>

--- Implementation/FindMyBLEDevice.Tests/BluetoothTests/BluetoothTests.cs
+++ Implementation/FindMyBLEDevice.Tests/BluetoothTests/BluetoothTests.cs
@@ -1,13 +1,50 @@
-﻿﻿// SPDX-License-Identifier: MIT
+﻿// SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2022 Nicolas Stellwag <nicolas.stellwag@fau.de>
 
+using FindMyBLEDevice.Models;
 using FindMyBLEDevice.Services.Bluetooth;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace FindMyBLEDevice.Tests.BluetoothTests
 {
+    [TestClass]
     public class BluetoothTests
     {
+        [TestMethod]
+        public async Task Search_EliminatesDuplicates()
+        {
+            // arrange
+            Guid id = Guid.Empty;
+            const string name = "some name";
+            const int rssi = 0;
+            var device = new Mock<IDevice>();
+            device.SetupGet(mock => mock.Id).Returns(id);
+            device.SetupGet(mock => mock.Name).Returns(name);
+            device.SetupGet(mock => mock.Rssi).Returns(rssi);
+            DeviceEventArgs args = new DeviceEventArgs();
+            args.Device = device.Object;
+
+            var adapter = new Mock<IAdapter>();
+            var bt = new Bluetooth(adapter.Object);
+
+            ObservableCollection<AvailableBTDevice> available = new ObservableCollection<AvailableBTDevice>();
+
+            // act
+            await bt.Search(100, available, null);
+            for (int i = 0; i < 3; i++)
+            {
+                adapter.Raise(mock => mock.DeviceDiscovered += null, args);
+            }
+
+            // assert
+            Assert.AreEqual(1, available.Count);
+            Assert.AreEqual(id, available[0].Id);
+        }
     }
 }
