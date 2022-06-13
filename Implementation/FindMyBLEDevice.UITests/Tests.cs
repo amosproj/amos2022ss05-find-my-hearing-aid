f13b878c2bd130e8403311aa61b64a272a5e54fa
Dominik Pysch <domi.pysch@gmail.com>
2022-06-13 23:07:20 +0200

    test: add first concrete (exemplary) UI-test
    Signed-off-by: Dominik Pysch <domi.pysch@gmail.com>

--- Implementation/FindMyBLEDevice.UITests/Tests.cs
+++ Implementation/FindMyBLEDevice.UITests/Tests.cs
@@ -25,24 +25,53 @@ namespace FindMyBLEDevice.UITests
             app = AppInitializer.StartApp(platform);
         }
 
+        /*
+        
+            Important Note: Mind that you always have to have installed the latest version of the app!
+                    Running tests does NOT push the app to the running emulator, it justs starts the existing version.
+
+            Some examples:
+
+            Interactive mode: useful for setting up tests.
+            `app.Repl();`
+
+            Search ("query") (until it shows up, in case of animations etc.) for UI-Elements via their "AutomationId". 
+            `AppResult[] results = app.WaitForElement(c => c.Marked("Btn_LearnMore"));`
+
+            Search ("query") for UI-Elements via their "AutomationId". 
+            `appResult[] results2 = app.Query(c => c.Marked("Btn_LearnMore"));`
+            `Assert.IsTrue(results.Any());`
+
+            Mimick user actions
+            `app.Tap(c => c.Marked("Btn_LearnMore"));`
+
+         */
+
+
         [Test]
-        public void WelcomeTextIsDisplayed()
+        public void OpenDevicesPage()
         {
 
-            // Interactive mode: useful for setting up tests.
-            // app.Repl();
-
-            // Search ("query") for UI-Elements via their "AutomationId". 
-            // AppResult[] results = app.WaitForElement(c => c.Marked("Btn_LearnMore"));
+            // Assert "About"-Screen is shown
             AppResult[] results = app.Query(c => c.Marked("Btn_LearnMore"));
             Assert.IsTrue(results.Any());
 
-            // Mimick user actions
-            app.Tap(c => c.Marked("Btn_LearnMore"));
+            // Tap on menu drawer (named "ok" for some reason..)
+            app.Tap(c => c.Marked("OK"));
 
-            // More asserts...
+            // Wait for drawer
+            AppResult[] results2 = app.WaitForElement(c => c.Marked("FlyoutItem_Devices"));
+            Assert.IsTrue(results2.Any());
 
-            
+            // Open devices page
+            app.Tap(c => c.Marked("FlyoutItem_Devices"));
+
+            // Assert that devices page (or at least one element from the page) is visible
+            AppResult[] results3 = app.WaitForElement(c => c.Marked("Label_SavedDevices"));
+            Assert.IsTrue(results3.Any());
+
         }
+
+
     }
 }
