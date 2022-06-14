9d1e282006397ebc92909538a2e0f5d6f12945e6
wandinad <adrian.wandinger@fau.de>
2022-06-14 20:40:55 +0200

    removed unused code components
    Co-authored-by: Marib Aldoais <marib.aldoais@gmail.com>
    Signed-off-by: wandinad <adrian.wandinger@fau.de>

--- Implementation/FindMyBLEDevice/FindMyBLEDevice/ViewModels/SettingsViewModel.cs
+++ Implementation/FindMyBLEDevice/FindMyBLEDevice/ViewModels/SettingsViewModel.cs
@@ -8,17 +8,6 @@ namespace FindMyBLEDevice.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
-        public Command SettingsCommand { get; }
-
-        public SettingsViewModel()
-        {
-            SettingsCommand = new Command(OnSettingsClicked);
-        }
-
-        private async void OnSettingsClicked(object obj)
-        {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
-        }
+        public SettingsViewModel() { }
     }
 }
