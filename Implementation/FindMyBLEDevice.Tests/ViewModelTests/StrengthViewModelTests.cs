f0319f659f1761fee9b9bff7c743bcbc77a7ba40
Nicolas Stellwag <nico.stellwag@gmail.com>
2022-07-12 15:25:30 +0200

    also use datarows for scaletest
    Signed-off-by: Nicolas Stellwag <nico.stellwag@gmail.com>

--- Implementation/FindMyBLEDevice.Tests/ViewModelTests/StrengthViewModelTests.cs
+++ Implementation/FindMyBLEDevice.Tests/ViewModelTests/StrengthViewModelTests.cs
@@ -57,10 +57,13 @@ namespace FindMyBLEDevice.Tests.ViewModelTests
         }
 
 
-        [TestMethod]
-        public void otherScaleToRadius_Test()
+        [DataTestMethod]
+        [DataRow(0, 10, 6, 36, 0)] // 36 = 60 * (6/10)
+        [DataRow(20, 200, 100, 27, 1)] // 27 = 60 * (80/180)
+        public void otherScaleToRadius_Test(double scaleMin, double scaleMax, double value, int expected, int acceptableDelta)
         {
             // arrange
+            const int radiusMin = 30;
             var disAcc = new Mock<IDeviceDisplayAccess>();
             disAcc.SetupGet(mock => mock.Width).Returns(1000);
             disAcc.SetupGet(mock => mock.Density).Returns(10);
@@ -70,17 +73,11 @@ namespace FindMyBLEDevice.Tests.ViewModelTests
 
             // act
             MethodInfo methodInfo = typeof(StrengthViewModel).GetMethod("otherScaleToRadius", BindingFlags.NonPublic | BindingFlags.Instance);
-            object[] parameters = { 0, 10, 6 };
-            int a = (int) methodInfo.Invoke(vm, parameters);
-            object[] parameters2 = { 20, 200, 100 };
-            int b = (int)methodInfo.Invoke(vm, parameters2);
+            object[] arguments = { scaleMin, scaleMax, value };
+            int res = (int) methodInfo.Invoke(vm, arguments);
 
             // assert
-            // 36 = 60 * (6/10)
-            Assert.AreEqual(30 + 36, a);
-            // 27 = 60 * (80/180)
-            Assert.AreEqual(30 + 27, b, 1);
-
+            Assert.AreEqual(radiusMin + expected, res, acceptableDelta);
         }
 
         [DataTestMethod]
