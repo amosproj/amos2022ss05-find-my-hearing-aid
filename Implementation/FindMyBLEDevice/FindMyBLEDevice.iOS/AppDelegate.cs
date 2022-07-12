83cfd32d1638443b5b990e253e5f5dd861af82a5
wandinad <adrian.wandinger@fau.de>
2022-07-12 15:02:13 +0200

    ios: added check for bluetooth permission
    Co-authored-by: Jannik Schuetz <jannik.schuetz@gmx.net>
    Signed-off-by: wandinad <adrian.wandinger@fau.de>

--- Implementation/FindMyBLEDevice/FindMyBLEDevice.iOS/AppDelegate.cs
+++ Implementation/FindMyBLEDevice/FindMyBLEDevice.iOS/AppDelegate.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Jannik Schuetz <jannik.schuetz@fau.de>
+// SPDX-FileCopyrightText: 2022 Adrian Wandinger <adrian.wandinger@fau.de>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using CoreBluetooth;
 using CoreLocation;
 using Foundation;
 using UIKit;
@@ -30,23 +35,45 @@ namespace FindMyBLEDevice.iOS
             Xamarin.FormsMaps.Init();
             LoadApplication(new App());
 
-            switch (CLLocationManager.Status)
+            bool hasBluetoothPermission = checkBluetoothPermission();
+            bool hasLocationPermission = checkLocationPermission();
+
+            if (!hasBluetoothPermission || !hasLocationPermission)
             {
-                case CLAuthorizationStatus.Authorized | CLAuthorizationStatus.AuthorizedAlways | CLAuthorizationStatus.AuthorizedWhenInUse:
-                    Console.WriteLine("Access");
-                    break;
-                default:
-                    Console.WriteLine("No Access");
-                    var message = "The app is not functioning correctly because the permissions are not granted. You will be redirected to the settings app to grant the required permissions";
-                    bool goToSettings = App.Current.MainPage.DisplayAlert("Attention", message, "Ok", "Cancel").GetAwaiter().GetResult();
-                    if (goToSettings)
-                    {
-                        Xamarin.Essentials.AppInfo.ShowSettingsUI();
-                    }
-                    break;
+                var message = "The app is not functioning correctly because the permissions are not granted. You will be redirected to the settings app to grant the required permissions";
+                bool goToSettings = App.Current.MainPage.DisplayAlert("Attention", message, "Ok", "Cancel").GetAwaiter().GetResult();
+                if (goToSettings)
+                {
+                    Xamarin.Essentials.AppInfo.ShowSettingsUI();
+                }
             }
 
+
             return base.FinishedLaunching(app, options);
+        }
+
+        bool checkLocationPermission()
+        {
+            switch (CLLocationManager.Status)
+            {
+                case CLAuthorizationStatus.Authorized:
+                case CLAuthorizationStatus.AuthorizedWhenInUse:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        bool checkBluetoothPermission()
+        {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                return CBCentralManager.Authorization == CBManagerAuthorization.AllowedAlways;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
