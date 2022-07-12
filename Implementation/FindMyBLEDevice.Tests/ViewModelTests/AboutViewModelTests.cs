5caa654fa74c5b5d570346be20a67ff389ea7921
Dominik Pysch <domi.pysch@gmail.com>
2022-07-12 11:51:09 +0200

    add some viewmodel-tests
    Signed-off-by: Dominik Pysch <domi.pysch@gmail.com>

--- Implementation/FindMyBLEDevice.Tests/ViewModelTests/AboutViewModelTests.cs
+++ Implementation/FindMyBLEDevice.Tests/ViewModelTests/AboutViewModelTests.cs
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2022 Leo Köberlein <leo@wolfgang-koeberlein.de>
+// SPDX-FileCopyrightText: 2022 Dominik Pysch <domi.pysch@gmail.com>
 
 using FindMyBLEDevice.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Threading.Tasks;
 
 namespace FindMyBLEDevice.Tests.ViewModelTests
 {
@@ -24,5 +26,47 @@ namespace FindMyBLEDevice.Tests.ViewModelTests
             Assert.IsNotNull(vm.OpenMapPageCommand);
             Assert.IsNotNull(vm.OpenStrengthPageCommand);
         }
+
+        [TestMethod]
+        public void OpenMapPageCommand_Works()
+        {
+            // arrange
+            AboutViewModel vm;
+            var nvg = new Mock<INavigator>();
+
+            nvg.SetupGet(mock => mock.MapPage).Returns("MapPage");
+            nvg.Setup(mock => mock.GoToAsync(It.IsAny<string>())).Returns(Task.CompletedTask);
+
+
+            // act
+            vm = new AboutViewModel(nvg.Object);
+            vm.OpenMapPageCommand.Execute(null);
+
+            // assert
+            nvg.Verify(mock => mock.GoToAsync(It.Is<string>(pageName =>
+               pageName == "MapPage")), Times.Once);
+
+        }
+
+        [TestMethod]
+        public void OpenStrengthPageCommand_Works()
+        {
+            // arrange
+            AboutViewModel vm;
+            var nvg = new Mock<INavigator>();
+
+            nvg.SetupGet(mock => mock.StrengthPage).Returns("StrengthPage");
+            nvg.Setup(mock => mock.GoToAsync(It.IsAny<string>())).Returns(Task.CompletedTask);
+
+            // act
+            vm = new AboutViewModel(nvg.Object);
+            vm.OpenStrengthPageCommand.Execute(null);
+
+            // assert
+            nvg.Verify(mock => mock.GoToAsync(It.Is<string>(pageName =>
+               pageName == "StrengthPage")), Times.Once);
+
+        }
+
     }
 }
--- Implementation/FindMyBLEDevice.Tests/ViewModelTests/StrengthViewModelTests.cs
+++ Implementation/FindMyBLEDevice.Tests/ViewModelTests/StrengthViewModelTests.cs
@@ -1,9 +1,15 @@
-﻿using FindMyBLEDevice.ViewModels;
+﻿// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2022 Nicolas Stellwag <nico.stellwag@gmail.com>
+// SPDX-FileCopyrightText: 2022 Dominik Pysch <domi.pysch@gmail.com>
+
+using FindMyBLEDevice.Models;
+using FindMyBLEDevice.ViewModels;
 using FindMyBLEDevice.XamarinAccess;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace FindMyBLEDevice.Tests.ViewModelTests
 {
@@ -33,5 +39,71 @@ namespace FindMyBLEDevice.Tests.ViewModelTests
                 Assert.AreEqual(reference, dist, 2);
             }
         }
+
+        [TestMethod]
+        public void MaxRadiusSmallerThanScreenWidth()
+        {
+            // arrange
+            var disAcc = new Mock<IDeviceDisplayAccess>();
+            disAcc.SetupGet(mock => mock.Width).Returns(1000);
+            disAcc.SetupGet(mock => mock.Density).Returns(10);
+
+            // act
+            var vm = new StrengthViewModel(disAcc.Object);
+
+            // assert
+            Assert.IsTrue(vm.CircleSizes.TrueForAll(s => s < (1000 / 10)));
+
+        }
+
+
+        [TestMethod]
+        public void otherScaleToRadius_Test()
+        {
+            // arrange
+            var disAcc = new Mock<IDeviceDisplayAccess>();
+            disAcc.SetupGet(mock => mock.Width).Returns(1000);
+            disAcc.SetupGet(mock => mock.Density).Returns(10);
+            // therefore MaxRadiusSize = 90
+            var vm = new StrengthViewModel(disAcc.Object);
+
+
+            // act
+            MethodInfo methodInfo = typeof(StrengthViewModel).GetMethod("otherScaleToRadius", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { 0, 10, 6 };
+            int a = (int) methodInfo.Invoke(vm, parameters);
+            object[] parameters2 = { 20, 200, 100 };
+            int b = (int)methodInfo.Invoke(vm, parameters2);
+
+            // assert
+            // 36 = 60 * (6/10)
+            Assert.AreEqual(30 + 36, a);
+            // 27 = 60 * (80/180)
+            Assert.AreEqual(30 + 27, b, 1);
+
+        }
+
+        [TestMethod]
+        public void rssiToMeter_SensibleResults()
+        {
+            // arrange
+            var disAcc = new Mock<IDeviceDisplayAccess>();
+            disAcc.SetupGet(mock => mock.Width).Returns(1000);
+            disAcc.SetupGet(mock => mock.Density).Returns(10);
+            var vm = new StrengthViewModel(disAcc.Object);
+
+            // act
+            MethodInfo methodInfo = typeof(StrengthViewModel).GetMethod("rssiToMeter", BindingFlags.NonPublic | BindingFlags.Instance);
+            object[] parameters = { -30, Constants.TxPowerDefault, Constants.RssiEnvironmentalDefault };
+            double a = (double) methodInfo.Invoke(vm, parameters);
+            object[] parameters2 = { -80, Constants.TxPowerDefault, Constants.RssiEnvironmentalDefault };
+            double b = (double) methodInfo.Invoke(vm, parameters2);
+
+            // assert
+            Assert.IsTrue(a > 0 && a < 10);            
+            Assert.IsTrue(b > 0 && b < 100);
+
+        }
+
     }
 }
